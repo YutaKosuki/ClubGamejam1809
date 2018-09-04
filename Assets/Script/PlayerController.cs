dbd92633bcc98d06ffef78cb26754f3584beb436
YutaKosuki <bambiyuta@gmail.com>
2018-09-04 15:49:34 +0900

    Add Script

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+ public float speed = 20;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
